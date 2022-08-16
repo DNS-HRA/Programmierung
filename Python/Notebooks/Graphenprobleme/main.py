f4b6102445ba3fcecc1817cd6e28bc773431ed71
DNS-HRA <44985923+DNS-HRA@users.noreply.github.com>
2022-08-16 14:43:15 +0200

    main hinzugefügt, um Code unabhängig vom Jupyter Notebook auszuführen

---
+++ Python/Notebooks/Graphenprobleme/main.py
@@ -0,0 +1,49 @@
+from Notebooks.Suche.generic_search import bfs, Node, node_to_path
+from graph import Graph
+from typing import TypeVar, Generic, List, Optional
+
+V = TypeVar('V')
+
+if __name__ == "__main__":
+    # Grundlegende Konstruktion des Graphen testen
+    city_graph: Graph[str] = Graph(
+        ["Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston", "New York", "Atlanta",
+         "Miami", "Dallas", "Houston", "Detroit", "Philadelphia", "Washington"])
+    city_graph.add_edge_by_vertices("Seattle", "Chicago")
+    city_graph.add_edge_by_vertices("Seattle", "San Francisco")
+    city_graph.add_edge_by_vertices("San Francisco", "Riverside")
+    city_graph.add_edge_by_vertices("San Francisco", "Los Angeles")
+    city_graph.add_edge_by_vertices("Los Angeles", "Riverside")
+    city_graph.add_edge_by_vertices("Los Angeles", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Chicago")
+    city_graph.add_edge_by_vertices("Phoenix", "Dallas")
+    city_graph.add_edge_by_vertices("Phoenix", "Houston")
+    city_graph.add_edge_by_vertices("Dallas", "Chicago")
+    city_graph.add_edge_by_vertices("Dallas", "Atlanta")
+    city_graph.add_edge_by_vertices("Dallas", "Houston")
+    city_graph.add_edge_by_vertices("Houston", "Atlanta")
+    city_graph.add_edge_by_vertices("Houston", "Miami")
+    city_graph.add_edge_by_vertices("Atlanta", "Chicago")
+    city_graph.add_edge_by_vertices("Atlanta", "Washington")
+    city_graph.add_edge_by_vertices("Atlanta", "Miami")
+    city_graph.add_edge_by_vertices("Miami", "Washington")
+    city_graph.add_edge_by_vertices("Chicago", "Detroit")
+    city_graph.add_edge_by_vertices("Detroit", "Boston")
+    city_graph.add_edge_by_vertices("Detroit", "Washington")
+    city_graph.add_edge_by_vertices("Detroit", "New York")
+    city_graph.add_edge_by_vertices("Boston", "New York")
+    city_graph.add_edge_by_vertices("New York", "Philadelphia")
+    city_graph.add_edge_by_vertices("Philadelphia", "Washington")
+    print(city_graph)
+
+    # Wie komme ich von Boston nach Miami? (Tiefensuche)
+    bfs_result: Optional[Node[V]] = bfs("Boston", lambda x: x == "Miami",
+                                        city_graph.neighbors_for_vertex)
+
+    if bfs_result is None:
+        print("Keine Lösung gefunden!")
+    else:
+        path: List[V] = node_to_path(bfs_result)
+        print("Pfad von Boston nach Miami: ")
+        print(path)
