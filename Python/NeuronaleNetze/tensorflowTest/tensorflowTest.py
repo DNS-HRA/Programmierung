dc53cc9618b453f3ab15ca0fefda485e38394dcb
DNS-HRA <44985923+DNS-HRA@users.noreply.github.com>
2022-08-25 03:35:24 +0200

    Ausgabe angepasst

--- Python/NeuronaleNetze/tensorflowTest/tensorflowTest.py
+++ Python/NeuronaleNetze/tensorflowTest/tensorflowTest.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-hello = tf.constant("Hello, Tensorflow!")
+hello = tf.constant("Hallo, Tensorflow!")
 tf.print(hello)
 print(hello)
 print("TensorFlow Version: {}".format(tf.__version__))
