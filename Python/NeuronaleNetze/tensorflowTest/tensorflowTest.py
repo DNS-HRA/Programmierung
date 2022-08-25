9c810c2319a638fd820421b2c94613bdaebfc411
DNS-HRA <44985923+DNS-HRA@users.noreply.github.com>
2022-08-25 02:53:10 +0200

    Formatiert nach PEP8

--- Python/NeuronaleNetze/tensorflowTest/tensorflowTest.py
+++ Python/NeuronaleNetze/tensorflowTest/tensorflowTest.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
+
 hello = tf.constant("Hello, Tensorflow!")
 tf.print(hello)
 print(hello)
 print("TensorFlow Version: {}".format(tf.__version__))
+# Tensorflow need to be enabled in the system
